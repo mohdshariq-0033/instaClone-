69e2287c142cac942763892f96eda36c4665cc02
mohd shariq <mohdshariq0033@gmail.com>
2025-03-16 15:37:28 +0530

    Add instaClone

---
+++ clone.js
@@ -0,0 +1,16 @@
+// input=document.querySelectorAll("input");
+// body=document.querySelector("#inCloneCont");
+// console.log(input);
+
+// input[0].addEventListener("click",()=>{
+//     input[0].style.border="1px solid red";
+// });
+
+// input[1].addEventListener("click",()=>{
+//     input[1].style.border="1px solid red";
+// });
+
+// body.addEventListener("click",()=>{
+//     body.style.border="1px solid rgb(22, 45, 177)";
+// });
+
